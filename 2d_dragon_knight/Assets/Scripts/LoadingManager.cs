72dab35027b2bdfb303631ad7ee1ff318502ec6b
CEK19 <nguyentrongnhan2001.work@gmail.com>
2022-04-04 07:41:32 +0700

    Add Menu when playing game by pressing esc button

--- 2d_dragon_knight/Assets/Scripts/LoadingManager.cs
+++ 2d_dragon_knight/Assets/Scripts/LoadingManager.cs
@@ -3,7 +3,37 @@ using UnityEngine.SceneManagement;
 
 public class LoadingManager : MonoBehaviour
 {
+    [SerializeField] private GameObject menuCanva;
+    private void Update() {
+        if (menuCanva != null) {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                if (menuCanva.activeInHierarchy == false) {
+                    Time.timeScale = 0;
+                    menuCanva.SetActive(true);
+                }
+
+                else if (menuCanva.activeInHierarchy == true) {
+                    Time.timeScale = 1;
+                    menuCanva.SetActive(false);
+                }   
+            }            
+        }
+    }
     public void PlayGame() {
         LoadingScence.instance.LoadingScenceFunc(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Continue() {
+        menuCanva.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void PlayAgain() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    public void Exit() {
+        Application.Quit();
     }
 }
